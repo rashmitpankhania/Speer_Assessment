35d8483649100c21c7a6ef84e0c771875394cf34
rashmitpankhania <rashmitpankhania@gmail.com>
2024-01-05 00:05:25 +0530

    add login api

--- routes/auth.js
+++ routes/auth.js
@@ -34,4 +34,36 @@ router.post('/signup', async function (req, res, next) {
     }
 });
 
+// login
+router.post("/login", async (req, res) => {
+
+    try {
+        const {email, password} = req.body;
+
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        const user = await User.findOne({email});
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // save user token
+            user.token = jwt.sign(
+                {user_id: user._id, email},
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "5h",
+                }
+            );
+
+            // user
+            return res.status(200).json(user);
+        }
+        return res.status(400).send("Invalid Credentials");
+
+        // Our login logic ends here
+    } catch (e) {
+        console.log(e);
+    }
+});
+
 module.exports = router;
