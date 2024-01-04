7674788f3bdf7e9198be5eb22834b8b8d8d2aeee
rashmitpankhania <rashmitpankhania@gmail.com>
2024-01-04 23:57:36 +0530

    add signup api

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,37 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../model/user');
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 /* Sign up. */
-router.post('/signup', function(req, res, next) {
-  const {firstName, lastName, email, password} = req.body;
-  if(!(email && password && firstName && lastName))
-    res.status(400).send('All input is needed!');
+router.post('/signup', async function (req, res, next) {
+    try {
+        const {firstName, lastName, email, password} = req.body;
+        if (!(email && password && firstName && lastName))
+            return res.status(400).send('All input is needed!');
+        const [existingUser] = await Promise.all([User.findOne({email}).exec()]);
+        if (existingUser)
+            return res.status(409).send('User already exist.')
 
-  const existingUser = User.findOne({email});
-  if(existingUser)
-    res.status(409).send('User already exist.')
+        let encryptedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            first_name: firstName,
+            last_name: lastName,
+            email: email.toLowerCase(), // sanitize
+            password: encryptedPassword,
+        });
+
+        user.token = jwt.sign(
+            {user_id: user._id, email},
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "5h",
+            }
+        );
+        res.status(201).json(user);
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 module.exports = router;
